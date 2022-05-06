9db319a58110a2da3e3fa600ae65abfee0eeabd1
flayner2 <flayner5@gmail.com>
2022-05-06 16:32:09 -0300

    Created a new component for the form filter boxes

---
+++ components/FilterBox.tsx
@@ -0,0 +1,15 @@
+import { Box, BoxProps } from '@chakra-ui/react';
+
+type Props = BoxProps;
+
+function FilterBox() {
+  return (
+    <Box
+      bg="tomato"
+      height="15vh"
+    ></Box>
+  );
+}
+
+export default FilterBox;
+
