6a0ce53794f6dab5c469f38a92d301bd4f195fe4
flayner2 <flayner5@gmail.com>
2022-05-09 15:50:24 -0300

    Changed the component to use Stacks and updated the styling

--- components/FilterBox.tsx
+++ components/FilterBox.tsx
@@ -1,11 +1,11 @@
-import { Text, Image, Flex, FlexProps } from '@chakra-ui/react';
+import { Text, Image, VStack, HStack, StackProps } from '@chakra-ui/react';
 
 interface Props {
   boxTitle?: string;
   sideImage?: string;
   imageAlt?: string;
   children?: any;
-  props?: FlexProps;
+  props?: StackProps;
 }
 
 function FilterBox({
@@ -16,30 +16,38 @@ function FilterBox({
   ...props
 }: Props) {
   return (
-    <Flex
-      height="20vh"
+    <VStack
+      height="10.3rem"
       bg="#f0f4f7"
       border="1px solid #92a2bc"
       borderRadius="8px"
-      justifyContent="space-between"
+      width="100%"
+      spacing="0"
       {...props}
     >
-      <Text
-        fontSize="sm"
-        lineHeight="16px"
-        color="#1e6ea6"
-        fontWeight="bold"
-        padding="8px"
+      <HStack
+        justify="space-between"
+        align="flex-start"
+        width="100%"
+        height="25%"
       >
-        {boxTitle}
-      </Text>
-      <Image
-        src={sideImage}
-        alt={imageAlt}
-        height="55%"
-      ></Image>
+        <Text
+          fontSize="sm"
+          lineHeight="16px"
+          color="#1e6ea6"
+          fontWeight="bold"
+          padding="8px"
+        >
+          {boxTitle}
+        </Text>
+        <Image
+          src={sideImage}
+          alt={imageAlt}
+          height="100%"
+        ></Image>
+      </HStack>
       {children}
-    </Flex>
+    </VStack>
   );
 }
 
