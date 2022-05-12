843d61c190437cf66c21b4729163c80b1b3195e1
flayner2 <flayner5@gmail.com>
2022-05-11 21:41:15 -0300

    Modified the api page to test requests to the database

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import {equal} from 'assert';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {prisma} from '../../lib/prisma';
 
-type Data = {
-  name: string
-}
+// type Data = {
+// name: string
+//}
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(
+    req: NextApiRequest, res: NextApiResponse<any>) {
+  const data = await prisma.geneIDToPMID.findMany({
+    where: {
+      pmid: 1393606,
+    },
+  });
+
+  res.status(200).json(data);
 }
