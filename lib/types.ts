599d0f55f629358e0629c32477877bba057422be
flayner2 <flayner5@gmail.com>
2022-05-19 11:52:05 -0300

    Added type interfaces for the 'debounce' function

--- lib/types.ts
+++ lib/types.ts
@@ -1,1 +1,13 @@
+export interface FunctionWithArguments {
+  (...args: any): any;
+}
 
+export interface DebouncedFunction<F extends FunctionWithArguments> {
+  (...args: Parameters<F>): Promise<ReturnType<F>>;
+}
+
+export interface DebounceReturn<F extends FunctionWithArguments> extends
+    Array<DebouncedFunction<F>|(() => void)> {
+  0: (...args: Parameters<F>) => Promise<ReturnType<F>>;
+  1: () => void;
+}
