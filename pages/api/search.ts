25e7acd79f70aca2d398bf04cd14e21089e09df4
flayner2 <flayner5@gmail.com>
2022-05-23 19:12:19 -0300

    Added support for an offset argument

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,23 +1,24 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {prisma} from '../../lib/prisma';
+import type {MetadataPub} from '@prisma/client';
 
-// type Data = {
-// name: string
-//}
-
-export async function getData(query: string) {
+export async function getData(query: string, offset: number = 0) {
   const data = await prisma.metadataPub.findMany({
     where: {title: {contains: query}},
     take: 20,
+    skip: offset,
   });
 
   return data;
 }
 
-async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+async function handler(req: NextApiRequest, res: NextApiResponse<MetadataPub>) {
   const title = Array.isArray(req.query.paperTitle) ? req.query.paperTitle[0] :
                                                       req.query.paperTitle;
-  const data = await getData(title);
+  const offset = parseInt(
+      Array.isArray(req.query.offset) ? req.query.offset[0] : req.query.offset);
+
+  const data = await getData(title, offset ? offset : 0);
 
   res.status(200).send(data);
 }
