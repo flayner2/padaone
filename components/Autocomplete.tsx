22e6aee713cce3821ff27f2d897c4104b37d136f
flayner2 <flayner5@gmail.com>
2022-05-23 19:11:50 -0300

    Started working on customizing the appearence of the autocomplete component

--- components/Autocomplete.tsx
+++ components/Autocomplete.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import type { ComboBoxProps } from '@react-types/combobox';
-import type { LoadingState } from '@react-types/shared';
-import { useComboBoxState } from 'react-stately';
-import { useComboBox, useFilter } from 'react-aria';
-import { Search2Icon } from '@chakra-ui/icons';
+//import type {ThemingProps} from '@chakra-ui/react';
 import {
+  Box,
+  FormLabel,
   Input,
   InputGroup,
-  InputRightElement,
-  FormLabel,
-  Box,
-  Spinner,
   InputLeftElement,
+  InputRightElement,
+  Spinner,
+  Text,
 } from '@chakra-ui/react';
-
+import type { ComboBoxProps } from '@react-types/combobox';
+import type { LoadingState } from '@react-types/shared';
+import * as React from 'react';
+import { useComboBox, useFilter } from 'react-aria';
+import { useComboBoxState } from 'react-stately';
 import { ListBox } from './ListBox';
 import { Popover } from './Popover';
 
@@ -22,6 +22,9 @@ export { Item, Section } from 'react-stately';
 interface AutocompleteProps<T> extends ComboBoxProps<T> {
   loadingState?: LoadingState;
   onLoadMore?: () => void;
+  button?: React.ReactNode;
+  placeholder?: string;
+  //inputSize?: ThemingProps<"Input">
 }
 
 export function Autocomplete<T extends object>(props: AutocompleteProps<T>) {
@@ -31,6 +34,8 @@ export function Autocomplete<T extends object>(props: AutocompleteProps<T>) {
   let inputRef = React.useRef(null);
   let listBoxRef = React.useRef(null);
   let popoverRef = React.useRef(null);
+
+  const { button } = props;
 
   let { inputProps, listBoxProps, labelProps } = useComboBox(
     {
@@ -49,23 +54,13 @@ export function Autocomplete<T extends object>(props: AutocompleteProps<T>) {
     >
       <FormLabel {...labelProps}>{props.label}</FormLabel>
       <InputGroup>
-        <InputLeftElement>
-          <Search2Icon color="gray.500" />
-        </InputLeftElement>
+        {button && <InputRightElement>{button}</InputRightElement>}
         <Input
           {...inputProps}
           ref={inputRef}
           size="md"
+          placeholder={props.placeholder}
         />
-        <InputRightElement>
-          {props.loadingState === 'loading' ||
-          props.loadingState === 'filtering' ? (
-            <Spinner
-              color="blue.400"
-              size="sm"
-            />
-          ) : null}
-        </InputRightElement>
       </InputGroup>
       {state.isOpen && (
         <Popover
@@ -73,13 +68,23 @@ export function Autocomplete<T extends object>(props: AutocompleteProps<T>) {
           isOpen={state.isOpen}
           onClose={state.close}
         >
-          <ListBox
-            {...listBoxProps}
-            listBoxRef={listBoxRef}
-            state={state}
-            loadingState={props.loadingState}
-            onLoadMore={props.onLoadMore}
-          />
+          {props.loadingState === 'loading' ||
+          props.loadingState === 'filtering' ? (
+            <Spinner
+              color="blue.400"
+              size="sm"
+            />
+          ) : state.collection.size ? (
+            <ListBox
+              {...listBoxProps}
+              listBoxRef={listBoxRef}
+              state={state}
+              loadingState={props.loadingState}
+              onLoadMore={props.onLoadMore}
+            />
+          ) : (
+            <Text>Term not found</Text>
+          )}
         </Popover>
       )}
     </Box>
