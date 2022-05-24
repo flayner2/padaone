e94c959a02787528c88a629ed1bc568367ee850c
flayner2 <flayner5@gmail.com>
2022-05-24 10:39:21 -0300

    Added theme colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,31 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import WithSubnavigation from '../components/Navbar';
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    protBlue: {
+      100: '#87b7fe',
+      300: '#0b57ee',
+      400: '#a2c5fa',
+      900: '#4a5568',
+      darkHover: '#222833',
+    },
+    protBlack: {
+      100: '#1a202c',
+    },
+    protGray: {
+      100: '#f5f5f5',
+      300: '#e2e8f0',
+      500: '#d9d9d9',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <WithSubnavigation></WithSubnavigation>
       <Component {...pageProps} />
     </ChakraProvider>
