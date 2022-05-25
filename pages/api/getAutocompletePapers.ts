37a93e2c0b48ba77287dfcde4cead9a31f19c0ea
flayner2 <flayner5@gmail.com>
2022-05-25 10:49:21 -0300

    Cleaned up the api route

--- pages/api/getAutocompletePapers.ts
+++ pages/api/getAutocompletePapers.ts
@@ -1,10 +1,8 @@
+import type {MetadataPub} from '@prisma/client';
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {prisma} from '../../lib/prisma';
-import type {MetadataPub} from '@prisma/client';
 
-type LanguagePub = string|null;
-
-export async function getData(
+export async function getPapersByTitle(
     query: string, offset: number = 0): Promise<MetadataPub[]> {
   const data = await prisma.metadataPub.findMany({
     where: {title: {contains: query}},
@@ -15,22 +13,6 @@ export async function getData(
   return data;
 }
 
-export async function getUniqueLanguages(): Promise<LanguagePub[]> {
-  const data = await prisma.metadataPub.findMany({
-    distinct: ['languagePub'],
-    select: {languagePub: true},
-    where: {
-      NOT: [{languagePub: null}],
-    },
-  });
-
-  const languages = data.map((languagePub) => languagePub.languagePub);
-
-  return languages;
-}
-
-export async function getJournals() {}
-
 async function handler(
     req: NextApiRequest, res: NextApiResponse<MetadataPub[]>) {
   const title = Array.isArray(req.query.paperTitle) ? req.query.paperTitle[0] :
@@ -38,7 +20,7 @@ async function handler(
   const offset = parseInt(
       Array.isArray(req.query.offset) ? req.query.offset[0] : req.query.offset);
 
-  const data = await getData(title, offset ? offset : 0);
+  const data = await getPapersByTitle(title, offset ? offset : 0);
 
   res.status(200).send(data);
 }
