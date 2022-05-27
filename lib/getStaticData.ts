5d37262012a25eb5fbc36d8ff199db45bd841139
flayner2 <flayner5@gmail.com>
2022-05-27 17:19:46 -0300

    Refactored the 'getClassificationLayersRange' function

--- lib/getStaticData.ts
+++ lib/getStaticData.ts
@@ -1,6 +1,8 @@
 import {Prisma} from '@prisma/client';
+
 import {prisma} from './prisma';
-import type {LanguagePub, MinMaxYearPub} from './types';
+
+import type {ClassificationRangeReturn, LanguagePub, MinMaxYearPub,} from './types';
 
 export async function getAllUniqueLanguages(): Promise<LanguagePub[]> {
   const data = await prisma.metadataPub.findMany({
@@ -29,4 +31,50 @@ export async function getPubDateRange():
   });
 
   return minMaxYear;
+}
+
+export async function getClassificationLayersRange():
+    Promise<ClassificationRangeReturn> {
+  const firstLayerRange = await prisma.classification1stLay.aggregate({
+    _max: {
+      probability: true,
+    },
+    _min: {
+      probability: true,
+    },
+  });
+
+  const secondLayerRange = await prisma.classification2ndLay.aggregate({
+    _max: {
+      probability: true,
+    },
+    _min: {
+      probability: true,
+    },
+  });
+
+  const classificationScores = {
+    firstLayer: {
+      min: firstLayerRange._min.probability ?
+          parseFloat(
+              (Number(firstLayerRange._min.probability) * 100).toFixed(2)) :
+          0,
+      max: firstLayerRange._max.probability ?
+          parseFloat(
+              (Number(firstLayerRange._max.probability) * 100).toFixed(2)) :
+          100,
+    },
+    secondLayer: {
+      min: secondLayerRange._min.probability ?
+          parseFloat(
+              (Number(secondLayerRange._min.probability) * 100).toFixed(2)) :
+          0,
+      max: secondLayerRange._max.probability ?
+          parseFloat(
+              (Number(secondLayerRange._max.probability) * 100).toFixed(2)) :
+          100,
+    },
+  };
+
+  return classificationScores;
 }
