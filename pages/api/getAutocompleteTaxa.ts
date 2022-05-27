562aca8fe902ae6c00d2eb4556e6a13813770b0d
flayner2 <flayner5@gmail.com>
2022-05-27 18:13:00 -0300

    Added an API endpoint for taxa autocomplete

---
+++ pages/api/getAutocompleteTaxa.ts
@@ -0,0 +1,51 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {prisma} from '../../lib/prisma';
+import type {TaxonNameAndID} from '../../lib/types';
+
+export async function getPapersByTitle(
+    query: string, offset: number = 0): Promise<TaxonNameAndID[]> {
+  const isID = !isNaN(parseInt(query));
+
+  const data = await prisma.taxIDToAccNumb.findMany({
+    distinct: ['taxID'],
+    where: {
+      OR: [
+        {orgTaxName: {contains: query}},
+        {taxID: isID ? parseInt(query) : -1},
+      ],
+    },
+    take: 20,
+    skip: offset,
+    select: {
+      orgTaxName: true,
+      taxID: true,
+    },
+  });
+
+  return data;
+}
+
+function parseTaxa(foundTaxa: TaxonNameAndID[]): TaxonNameAndID[] {
+  return foundTaxa.map(
+      (taxonObject) => ({
+        orgTaxName: `${taxonObject.orgTaxName} (${taxonObject.taxID})`,
+        taxID: taxonObject.taxID,
+      }));
+}
+
+async function handler(
+    req: NextApiRequest, res: NextApiResponse<TaxonNameAndID[]>) {
+  const taxonName = Array.isArray(req.query.taxonName) ?
+      req.query.taxonName[0] :
+      req.query.taxonName;
+  const offset = parseInt(
+      Array.isArray(req.query.offset) ? req.query.offset[0] : req.query.offset);
+
+  const data = await getPapersByTitle(taxonName, offset ? offset : 0);
+
+  const taxaWithPrettyNames = parseTaxa(data);
+
+  res.status(200).send(taxaWithPrettyNames);
+}
+
+export default handler;
