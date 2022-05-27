e9bf5e138a3ad5584b9fdc2f92eed3b1bba6873a
flayner2 <flayner5@gmail.com>
2022-05-27 17:27:11 -0300

    Added a helper function to convert probability ranges from prisma to floating point numbers

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,6 +1,15 @@
+import {Prisma} from '@prisma/client';
+
 export function filterYears(
     date: Date, minYear: number, maxYear: number): boolean {
   const year = date.getFullYear();
 
   return year >= minYear || year <= maxYear;
+}
+
+export function convertToFloatOrDefault(
+    original: number|Prisma.Decimal|null, precision: number, factor: number = 1,
+    fallback: number = 0): number {
+  return original ? parseFloat((Number(original) * factor).toFixed(precision)) :
+                    fallback;
 }
