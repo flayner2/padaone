7d6bd8e3c34057eb96ff232476327a5130cfbe02
flayner2 <flayner5@gmail.com>
2022-06-06 14:20:49 -0300

    Added request sanitation, error handling and improved responses

--- pages/api/getAutocompletePapers.ts
+++ pages/api/getAutocompletePapers.ts
@@ -18,18 +18,50 @@ export async function getPapersByTitle(
 }
 
 async function handler(
-    req: NextApiRequest, res: NextApiResponse<PaperTitlePMID[]>) {
+    req: NextApiRequest, res: NextApiResponse<PaperTitlePMID[]|Error>) {
   if (req.method !== 'GET') {
-    res.status(405).send(Error(`Method ${req.method} is not allowed.`));
+    res.status(405).send(new Error(`Method ${req.method} is not allowed.`));
   }
-  const title = Array.isArray(req.query.paperTitle) ? req.query.paperTitle[0] :
-                                                      req.query.paperTitle;
-  const offset = parseInt(
-      Array.isArray(req.query.offset) ? req.query.offset[0] : req.query.offset);
 
-  const data = await getPapersByTitle(title, offset ? offset : 0);
+  if (!req.query.paperTitle.length) {
+    res.status(400).send(
+        new Error('Query parameter "paperTitle" is required.'));
+  }
 
-  res.status(200).send(data);
+  if (Array.isArray(req.query.paperTitle)) {
+    res.status(400).send(
+        new Error('Query parameter "paperTitle" may contain only one value.'));
+  } else {
+    if (Array.isArray(req.query.offset)) {
+      res.status(400).send(
+          new Error('Query parameter "offset" may contain only one value.'));
+    } else {
+      const paper = req.query.paperTitle;
+      const offset = parseInt(req.query.offset);
+
+      if (!offset && req.query.offset.length) {
+        res.status(400).send(new Error(
+            'Query parameter "offset" must be either empty or a numeric value.'));
+      }
+
+      try {
+        const data = await getPapersByTitle(paper, offset ? offset : 0);
+
+        res.status(200).send(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.name === 'NotFoundError') {
+            res.status(400).send({
+              ...error,
+              message: `The requested paper with title ${paper} was not found.`,
+            });
+          } else {
+            res.status(500).send(error);
+          }
+        }
+      }
+    }
+  }
 }
 
 export default handler;
