9083d79f96ba1af7e1022980320a191b30446c82
Maycon Oliveira <flayner5@gmail.com>
2022-07-01 14:34:58 -0400

    added favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,6 +40,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>PADA-One</title>
+	<link rel="shortcut icon" href="/images/logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png"/>
       </Head>
       <SSRProvider>
         <ChakraProvider theme={theme}>
